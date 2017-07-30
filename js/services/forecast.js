6b8f338b683b1ae72ad1ec20863a517cc8f5f8fc
Alex <alex@Alexs-MBP.fios-router.home>
2017-07-30 00:40:45 -0400

    minor

--- js/services/forecast.js
+++ js/services/forecast.js
@@ -1,6 +1,6 @@
 app.factory('forecast', ['$http', function($http){
 	return 
-	$http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json)
+	$http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json')
 		.success(function(data){
 			return data;
 		})
